fa7b8c689f92e57f987679c6fe854580d6845490
Ismaël Lachheb <ismael.lachheb@protonmail.com>
2020-07-10 22:54:01 +0200

    test batch generator

---
+++ tests/test_generator_batch.py
@@ -0,0 +1,22 @@
+# on peut fournir au réseaux de neurone un ensemble de batch pris aléatoirement avec "sampling".
+
+
+def test_generator_batch_return_length_of_dataframe_on_one_epoch():
+    # given
+    dataset = [[4,3], [3,3], [1,1], [0,1], [0,0]]
+    batch_size = 2
+    batchgenerator = BatchGenerator(batch_size,dataset)
+    expected_len = 5
+    expected_iteration_number = len(dataset) % batch_size 
+
+    # when 
+    output_len = 0
+    iteration_number  = 0
+    while batchgenerator.is_next_batch() :
+        ouput_len += len(batchgenerator.next_batch())
+        iteration_number += 1
+
+
+    # then
+    assert ouput_len == expected_len
+    assert iteration_number == expected_iteration_number
